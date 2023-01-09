19c2c4d96fdad587f65b76caf82047959ff84a71
stm33 <satuma33@icloud.com>
2023-01-09 21:32:36 +0900

    [add]refs #4-beginning-design

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-type Book struct {
+import (
+	"errors"
+	"fmt"
+)
+
+type Money struct {
+	amount   int
+	currency Currency
+}
+
+type Currency struct {
+	value int
+}
+
+func NewMoney(amount int, currency Currency) (*Money, error) {
+	if amount < 0 {
+		return nil, errors.New("ERROR")
+	}
+	return &Money{amount, currency}, nil
+}
+
+/*func (m *Money) add(other int) {
+	m.amount = m.amount + other
+}*/
+
+func (m *Money) add(other int) *Money {
+	added := m.amount + other
+	return &Money{added, m.currency}
 }
 
 func main() {
+	cur := Currency{}
+	mon, err := NewMoney(3, cur)
+	if err != nil {
+		fmt.Println("ERROR")
+	}
+	mon2 := mon.add(3)
+	fmt.Println(mon2)
+	fmt.Println(mon)
 }
